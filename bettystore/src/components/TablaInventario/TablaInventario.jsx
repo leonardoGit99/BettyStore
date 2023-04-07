b1f7bcbf08f431a5bcc526f4b9f2c7c66636f3ae
Leonardo Fuentes Claros <51714281+leonardoGit99@users.noreply.github.com>
2023-04-07 18:09:34 -0400

    Peticion Get para tabla inventario exitosa. indexPHP aun no esta en el proyecto

--- bettystore/src/components/TablaInventario/TablaInventario.jsx
+++ bettystore/src/components/TablaInventario/TablaInventario.jsx
@@ -7,13 +7,9 @@ import './TablaInventarioStyle.css';
 
 
 function TablaInventario(props) {
-
-  //const [data, setData] = useState([]);
-  const peticionUrl = "http://localhost/crudProductos/indexConsultaGeneral.php";
-
-
+  const peticionGetAPI = "http://localhost/crudProductos/index.php";
+  const peticionDeleteAPI = "http://localhost/crudProductos/index.php?borrar";
   const [modalEliminar, setModalEliminar] = useState(false);
-
   //Accion de abrir y cerrar modal de eliminacion
   const abrirCerrarModalEliminar = () => {
     setModalEliminar(!modalEliminar);
@@ -31,14 +27,14 @@ function TablaInventario(props) {
       key: "nomProd",
     },
     {
-      title: "Descripcion",
-      dataIndex: "descripcionProd",
-      key: "descripcionProd",
-    },
-    {
       title: "Categoria",
       dataIndex: "categoriaProd",
       key: "categoriaProd",
+    },
+    {
+      title: "Descripcion",
+      dataIndex: "descripcionProd",
+      key: "descripcionProd",
     },
     {
       title: "Precio (Bs)",
@@ -51,14 +47,14 @@ function TablaInventario(props) {
       key: "cantidadProd",
     },
     {
-      title: "Imagen",
-      dataIndex: "imagenProd",
-      key: "imagenProd",
-    },
-    {
       title: "Fecha",
       dataIndex: "fechaProd",
       key: "fechaProd",
+    },
+    {
+      title: "Imagen",
+      dataIndex: "imagenProd",
+      key: "imagenProd",
     },
     {
       title: "Opciones",
@@ -66,83 +62,13 @@ function TablaInventario(props) {
       render: (fila) => (<Button className='btnEliminar' type='primary' danger onClick={abrirCerrarModalEliminar} icon={<DeleteOutlined />}></Button>),
     },
   ];
-  /*
-    const data=[
-      {
-        key: '1', 
-        codigo: '1234567891011',
-        nombre: "Coca Cola",
-        descripcion: "Coca-Cola es una bebida azucarada gaseosa vendida a nivel mundial en tiendas, restaurantes y máquinas expendedoras en más de doscientos países o territorios",
-        categoria: "Aguas y bebidas",
-        precio: "13",
-        cantidad: "24",
-        imagen: "X img preview",
-        fecha: "DD/AA/MM",
-      },
-      {
-        key: '2', 
-        codigo: '1286869891011',
-        nombre: "Leche Pil 1L",
-        descripcion: "Es un producto elaborado a partir de leche seleccionada y controlada, la cual es higienizada, estandarizada al 3,0% de materia grasa, homogenizada y ultrapasteurizada (UHT)",
-        categoria: "Lacteos",
-        precio: "6",
-        cantidad: "57",
-        imagen: "X img preview",
-        fecha: "DD/AA/MM",
-      },
-      {
-        key: '3', 
-        codigo: '7878767891011',
-        nombre: "Bacardi Manzana",
-        descripcion: "Ron blanco con sabor a manzana verde Granny Smith, Golden Delicious y Fuji. De sabor refrescante y suavemente cítrico, idealmente para mezclar con bebida tipo ginger ale o refrescos de limón.",
-        categoria: "Licoreria",
-        precio: "80",
-        cantidad: "7",
-        imagen:  "X preview",
-        fecha: "DD/AA/MM",
-      },
-      {
-        key: '4', 
-        codigo: '7878767891011',
-        nombre: "Bacardi Manzana",
-        descripcion: "Ron blanco con sabor a manzana verde Granny Smith, Golden Delicious y Fuji. De sabor refrescante y suavemente cítrico, idealmente para mezclar con bebida tipo ginger ale o refrescos de limón.",
-        categoria: "Licoreria",
-        precio: "80",
-        cantidad: "7",
-        imagen:  "X preview",
-        fecha: "DD/AA/MM",
-      },
-      {
-        key: '5', 
-        codigo: '7878767891011',
-        nombre: "Bacardi Manzana",
-        descripcion: "Ron blanco con sabor a manzana verde Granny Smith, Golden Delicious y Fuji. De sabor refrescante y suavemente cítrico, idealmente para mezclar con bebida tipo ginger ale o refrescos de limón.",
-        categoria: "Licoreria",
-        precio: "80",
-        cantidad: "7",
-        imagen:  "X preview",
-        fecha: "DD/AA/MM",
-      },
-      {
-        key: '6', 
-        codigo: '7878767891011',
-        nombre: "Bacardi Manzana",
-        descripcion: "Ron blanco con sabor a manzana verde Granny Smith, Golden Delicious y Fuji. De sabor refrescante y suavemente cítrico, idealmente para mezclar con bebida tipo ginger ale o refrescos de limón.",
-        categoria: "Licoreria",
-        precio: "80",
-        cantidad: "7",
-        imagen:  "X preview",
-        fecha: "DD/AA/MM",
-      },
-    ];
-  */
 
   //  Peticion Get de la API usando axios.
 
   const peticionGet = async () => {
-    await axios.get(peticionUrl)
+    await axios.get(peticionGetAPI)
       .then(response => {
-        props.setDatosTabla(response.data.list);
+        props.setDatosTabla(response.data);
         console.log(response);
       }).catch(error => {
         console.log(error);
@@ -150,15 +76,13 @@ function TablaInventario(props) {
   }
 
   //uso de useEffect para poder llamar a la peticion
-
   useEffect(() => {
     peticionGet();
   }, [])
 
   //  Peticion delete a la API usando axios.
-
   const peticionDelete = async () => {
-    await axios.delete(peticionUrl + "/" + props.producto.id)
+    await axios.delete(peticionDeleteAPI + props.producto.id)
       .then(response => {
         props.setDatosTabla(props.datosTabla.filter(elemento => elemento.id !== props.producto.id));
         infoEliminado();
@@ -177,7 +101,7 @@ function TablaInventario(props) {
     <div className='mostrarInventario'>
       <h2 className='subtituloTabla'>Productos Registrados en Inventario</h2>
       {/* TablaDinamica */}
-      <Table className='tabla' rowKey='id' columns={columnas} dataSource={props.datosTabla} bordered={true} pagination={{ pageSize: 5, pagination: true, position: ["bottomCenter"] }} size={'small'} />\
+      <Table className='tabla' rowKey='codProd' columns={columnas} dataSource={props.datosTabla} bordered={true} pagination={{ pageSize: 5, pagination: true, position: ["bottomCenter"] }} size={'small'} />\
 
       {/* TablaEstatica */}
       {/* <Table className='tabla'columns={columnas} dataSource={data} bordered={true} pagination={{pageSize: 5, pagination: true, position: ["bottomRight"]}}  size={'small'}/> */}

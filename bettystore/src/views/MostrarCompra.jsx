7b5d264203fb39e3851f8dc269f1972a3f74e266
Vicente_Rico <vicentestefano4042unknowedtrue@gmail.com>
2023-04-19 19:21:41 -0400

    Implementar vista Registrar Compra y Mostrar Compra, actualizando el menú con elementos de Compra

---
+++ bettystore/src/views/MostrarCompra.jsx
@@ -0,0 +1,13 @@
+import { Row, Col} from "antd";
+
+export default function MostrarCompra(props){
+  return(
+      <div>
+        <Row>
+            <Col >
+                <h1>Mostrar Compra</h1>
+            </Col>
+          </Row>           
+      </div>
+  )
+}

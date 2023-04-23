51ec1415c276044451eb9e43edf0c722207f7926
ferflooo27 <128374862+ferflooo27@users.noreply.github.com>
2023-04-23 19:14:49 -0400

    agregando restricciones, modificando calendario a español

--- bettystore/src/views/RegistrarCompra.jsx
+++ bettystore/src/views/RegistrarCompra.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Col, Row, DatePicker, Table, AutoComplete } from "antd";
+import { Form, Input, Button, Col, Row, DatePicker, Table, InputNumber, AutoComplete } from "antd";
 import dayjs from "dayjs";
 import { ShoppingCartOutlined } from '@ant-design/icons'
+import SearchOutlined from '@ant-design/icons/SearchOutlined';
+import 'dayjs/locale/es';
+
+
 import axios from "axios";
+dayjs.locale('es');
 
 export default function RegistrarCompra() {
 
@@ -74,7 +79,7 @@ export default function RegistrarCompra() {
   const disabledDate = (current) => {
     // Can not select days before today and today
 
-    return current && current < dayjs();
+    return current && current < dayjs().subtract(1,'day').endOf('day');
   };
 
   const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
@@ -109,16 +114,19 @@ export default function RegistrarCompra() {
               onSelect={handleSelect}
               placeholder="Busque un producto del inventario"
               onSearch={handleSearch}
+              
             >
               <Input
-                size="large"
+                size="middle"
+                suffix={<SearchOutlined />}
               />
             </AutoComplete>
             {/* Boton para mostrar el producto seleccionado por consola */}
             <Button onClick={mostrarSeleccionado}>Mostrar seleccionado por consola</Button>
           </Row>
           <p></p> {/* Esto estaría bien así o manejarlo como una Row*/}
-          <Row>
+          <Form>
+          <Row>  
             <Col lg={12}>
               <Item
                 label="Producto Seleccionado: "
@@ -131,7 +139,7 @@ export default function RegistrarCompra() {
                 </Input>
               </Item>
             </Col>
-            <Col lg={2}></Col>
+            <Col lg={3}></Col>
             <Col lg={6} >
               <Item
                 label="Fecha de Compra"
@@ -144,32 +152,83 @@ export default function RegistrarCompra() {
                 <DatePicker name="fechaProd"
                   placeholder="DD/MM/AAAA"
                   disabledDate={disabledDate}
+                  locale={{lang: {locale: 'es', ok: 'Aceptar', cancel: 'Cancelar'}}}
                   format={dateFormatList}
                   onChange={handleChangeDate} />
               </Item>
             </Col>
+            
           </Row>
           <Row>
-            <Col lg={8}>
+            <Col lg={6}>
               <Item
                 label="Cantidad: "
+                name ='cantCompra'
+                rules={[{
+                  required: true,
+                  message: "Por favor, seleccione una cantidad",
+                },
+                {
+                  whitespace: true,
+                  message: 'No puede dejar en blanco este campo',
+                },
+                {
+                  validator: (_, value) =>
+                    value && value.match('^0*[1-9][0-9]*$')
+                      ? Promise.resolve()
+                      : Promise.reject(new Error('Debe ingresar solo números y un valor mayor a cero')),
+                },
+                ]}
               >
                 <Input
                   className="cantidadProducto"
                   name="cantProd"
                   placeholder="#"
-                  maxLength={3}
+                  showCount
+                  maxLength={4}
+                />
+              </Item>
+            </Col>
+            <Col lg={1}></Col>
+            <Col lg={6}>
+              <Item
+                label="Codigo Compra: "
+                name ='codCompra'
+                rules={[{
+                  required: true,
+                  message: "Por favor, ingrese un codigo",
+                },
+                {
+                  whitespace: true,
+                  message: 'No puede dejar en blanco este campo',
+                },
+                {
+                  validator: (_, value) =>
+                    value && value.match('^0*[1-9][0-9]*$')
+                      ? Promise.resolve()
+                      : Promise.reject(new Error('Debe ingresar solo números y un valor mayor a cero')),
+                },
+                ]}
+              >
+                <Input
+                  className="cantidadProducto"
+                  name="cantProd"
+                  placeholder="#"
+                  showCount
+                  maxLength={4}
                 />
               </Item>
             </Col>
             <Col lg={2}></Col>
+
             <Col lg={6} >
               {/* Boton Agregar al detalle de compras */}
               <Button
-                icon={<ShoppingCartOutlined />} >Agregar
+                icon={<ShoppingCartOutlined />} htmlType="submit" >Agregar
               </Button>
             </Col>
           </Row>
+          </Form>
         </Col>
         <Col lg={2}></Col>
       </Row>

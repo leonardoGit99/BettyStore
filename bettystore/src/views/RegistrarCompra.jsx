7f32d35a7b5a2546be8b662dc3d4f51c5ae15024
Vicente_Rico <vicentestefano4042unknowedtrue@gmail.com>
2023-04-25 22:03:48 -0400

    Implementando Modal para formulario de registro de compras

--- bettystore/src/views/RegistrarCompra.jsx
+++ bettystore/src/views/RegistrarCompra.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Col, Row, DatePicker, Table, AutoComplete, message, } from "antd";
+import { Form, Input, Button, Col, Row, DatePicker, Table, AutoComplete, message, Modal } from "antd";
 import dayjs from "dayjs";
 import { ShoppingCartOutlined } from '@ant-design/icons'
 import { DeleteOutlined } from '@ant-design/icons';
@@ -109,6 +109,7 @@ export default function RegistrarCompra() {
     };
     setComprasTotales([...comprasTotales, nuevoProducto]);
     form.resetFields();
+    cerrarModal();
   };
 
   const eliminarProductoDetalleCompras = (key) => {
@@ -116,6 +117,17 @@ export default function RegistrarCompra() {
     setComprasTotales(productoAEliminarDelDetalleCompra);
   };
 
+
+  //Modal
+  const [modalEsVisible, setModalEsVisible] = useState(false);
+
+  const mostrarModal = () => {
+    setModalEsVisible(true);
+  }
+
+  const cerrarModal = () => {
+    setModalEsVisible(false);
+  }
 
   return (
     <div>
@@ -127,91 +139,103 @@ export default function RegistrarCompra() {
         <Col lg={2}></Col>
       </Row>
       <Row>
+        <Modal
+          title="Agrega un Producto al detalle de compras"
+          open={modalEsVisible}
+          onCancel={cerrarModal}
+          footer={null}
+          width={1270}
+        >
+          {/*<Layout></Layout>*/}
+          <Col lg={2} xs={2}></Col>
+          <Col lg={20} xs={20} className="componentsContainer">
+            <Row>
+              <Col lg={1}></Col>
+              <Col lg={10}>
 
-        {/*<Layout></Layout>*/}
-        <Col lg={2} xs={2}></Col>
-        <Col lg={20} xs={20} className="componentsContainer">
-          <Row>
-            <Col lg={1}></Col>
-            <Col lg={10}>
+                {/* Buscador de inventario */}
+                <AutoComplete
+                  style={{ width: 750 }}
+                  options={productos.map((producto) => ({ value: producto.nomProd, cantidadProd: producto.cantidadProd, precioProd: producto.precioProd, codProd: producto.codProd }))}
+                  onSelect={handleSelect}
+                  placeholder="Busque un producto del inventario"
+                  onSearch={handleSearch}
+                >
+                  <Input
+                    size="large"
+                  />
+                </AutoComplete>
+                {/* Boton para mostrar el producto seleccionado por consola */}
+                {/*<Button onClick={mostrarSeleccionado}>Mostrar seleccionado por consola</Button>*/}
+              </Col>
+            </Row>
+            <p></p> {/* Esto estaría bien así o manejarlo como una Row*/}
+            <Row>
+              <Col span={1}></Col>
+              {/*Columna para todo el formulario*/}
+              <Col span={22}>
+                <Form form={form} onFinish={agregarAlDetalleDeCompras} layout="inline">
+                  <Col lg={10}>
+                    <Form.Item label="Producto Seleccionado: ">
+                      <Input
+                        style={{ color: "#676767" }}
+                        disabled
+                        placeholder="Ningun producto seleccionado"
+                        value={seleccionado.value}
+                        name="nombre"
+                        rules={[{ required: true }]}
+                      >
 
-              {/* Buscador de inventario */}
-              <AutoComplete
-                style={{ width: 750 }}
-                options={productos.map((producto) => ({ value: producto.nomProd, cantidadProd: producto.cantidadProd, precioProd: producto.precioProd, codProd: producto.codProd }))}
-                onSelect={handleSelect}
-                placeholder="Busque un producto del inventario"
-                onSearch={handleSearch}
-              >
-                <Input
-                  size="large"
-                />
-              </AutoComplete>
-              {/* Boton para mostrar el producto seleccionado por consola */}
-              {/*<Button onClick={mostrarSeleccionado}>Mostrar seleccionado por consola</Button>*/}
-            </Col>
-          </Row>
-          <p></p> {/* Esto estaría bien así o manejarlo como una Row*/}
-          <Row>
-            <Col span={1}></Col>
-            {/*Columna para todo el formulario*/}
-            <Col span={22}>
-              <Form form={form} onFinish={agregarAlDetalleDeCompras} layout="inline">
-                <Col lg={10}>
-                  <Form.Item label="Producto Seleccionado: ">
-                    <Input
-                      style={{ color: "#676767" }}
-                      disabled
-                      placeholder="Ningun producto seleccionado"
-                      value={seleccionado.value}
-                      name="nombre"
-                      rules={[{ required: true }]}
-                    >
+                      </Input>
+                    </Form.Item>
+                  </Col>
+                  <Col lg={1}></Col>
+                  <Col lg={6}>
+                    <Form.Item label="Fecha" name="fecha" rules={[{ required: true }]}>
+                      <DatePicker format="YYYY-MM-DD" />
+                    </Form.Item>
+                  </Col>
 
-                    </Input>
-                  </Form.Item>
-                </Col>
-                <Col lg={1}></Col>
-                <Col lg={6}>
-                  <Form.Item label="Fecha" name="fecha" rules={[{ required: true }]}>
-                    <DatePicker format="YYYY-MM-DD" />
-                  </Form.Item>
-                </Col>
+                  <Col lg={6}>
+                    <Form.Item label="Cantidad" name="cantidad" rules={[{ required: true }]}>
+                      <Col lg={10}>
+                        <Input type="number" min="0" step="1" />
+                      </Col>
+                    </Form.Item>
+                  </Col>
 
-                <Col lg={6}>
-                  <Form.Item label="Cantidad" name="cantidad" rules={[{ required: true }]}>
-                    <Col lg={10}>
-                      <Input type="number" min="0" step="1" />
-                    </Col>
-                  </Form.Item>
-                </Col>
-
-                <Col lg={6}>
-                  <Form.Item label="Código Compra" name="codigoCompra" rules={[{ required: true }]}>
-                    <Col lg={10}>
-                      <Input />
-                    </Col>
-                  </Form.Item>
-                </Col>
-                <Col lg={6}>
-                  <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                      Agregar
-                    </Button>
-                  </Form.Item>
-                </Col>
-              </Form>
-            </Col>
-            <Col span={1}></Col>
-          </Row>
-        </Col>
-        <Col lg={2}></Col>
+                  <Col lg={6}>
+                    <Form.Item label="Código Compra" name="codigoCompra" rules={[{ required: true }]}>
+                      <Col lg={10}>
+                        <Input />
+                      </Col>
+                    </Form.Item>
+                  </Col>
+                  <Col lg={6}>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit">
+                        Agregar
+                      </Button>
+                    </Form.Item>
+                  </Col>
+                </Form>
+              </Col>
+              <Col span={1}></Col>
+            </Row>
+          </Col>
+          <Col lg={2}></Col>
+        </Modal>
       </Row>
 
       <Row>
         {/*<Layout></Layout>*/}
         <Col lg={2}></Col>
         <Col lg={20} xs={24} className="componentsContainer">
+          <Col lg={2}></Col>
+          <Col lg={20}>
+            <Button type="primary" onClick={mostrarModal}>Agregar Producto</Button>
+          </Col>
+          <Col lg={2}></Col>
 
           {/* Tabla detalle de compras */}
           <h2 className='subtituloTablaDetalleCompras'>Detalle de compra</h2>

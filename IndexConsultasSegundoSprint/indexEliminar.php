8f58aeb8bf02938d3c93aae0871940f71760f984
Agustin29gz <timbergdj200@gmail.com>
2023-04-29 23:25:39 -0400

    Insercion en multiple en Detalle de Compra en conjunto con Cenia Choquevillca y Nicolas Torrico

---
+++ IndexConsultasSegundoSprint/indexEliminar.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Conecta a la base de datos  con usuario, contraseña y nombre de la BD
+$servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "bettystorebd";
+$conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
+
+//Elimina un registro de acuerdo a un codigo
+if (isset($_GET["borrar"])){
+
+    $sqlProducto = mysqli_query($conexionBD,"DELETE FROM producto WHERE codProd=".$_GET["borrar"]." AND NOT EXISTS (SELECT * FROM detallecompra WHERE Producto_codProducto=".$_GET["borrar"].")");
+    
+    if($sqlProducto){
+        echo json_encode("Producto eliminado");
+        exit();
+    }else{ 
+        echo json_encode("Error al eliminar");
+    }
+}
+
+?>

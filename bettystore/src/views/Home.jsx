00768bc88373d89b80919217a939d774879cd040
Vicente_Rico <vicentestefano4042unknowedtrue@gmail.com>
2023-05-01 12:15:45 -0400

    Solucionando bug falto añadir componente Footer al Home Page

--- bettystore/src/views/Home.jsx
+++ bettystore/src/views/Home.jsx
@@ -1,20 +1,54 @@
-import {Row, Col} from 'antd'
+import { Link } from "react-router-dom"
+import { Row, Col, Card, Popover } from 'antd'
 //import SignIn from '../components/SignIn'
+import Footer from "../components/Footer/Footer";
 
 
-export default function Home(){
-    return(
-        <Row>   
-            <Col span={18}>
-                <div>
-                    <h1 lg={6} md={24} xs={24}>Home page de BettyStore</h1>
-                    <h2>Página en construcción :)</h2>
-                    <img className='homeEnDesarrollo' src='./assets/homePageEnDesarrollo.jpg'></img>
-                </div>
-            </Col>
-            <Col span={6}>
-                {/*<SignIn/>*/}
-            </Col>
-        </Row>     
+function redireccionarRegistrarProducto() {
+    window.location.href = "/registrarProducto";
+}
+
+function redireccionarMostrarInventario() {
+    window.location.href = "/mostrarInventario";
+}
+
+function redireccionarRegistrarCompra() {
+    window.location.href = "/registrarCompra";
+}
+
+function redireccionarMostrarCompra() {
+    window.location.href = "/mostrarCompra";
+}
+
+export default function Home() {
+    return (
+        <div>
+            <Row>
+                <Col span={8}></Col>
+                <Col span={8}>
+                    <div>
+                        <h1 lg={24} md={24} xs={24} className='tituloHome'>BettyStore</h1>
+                        {/*<h2>Página en construcción :)</h2>
+                    <img className='homeEnDesarrollo' src='./assets/homePageEnDesarrollo.jpg'></img>*/}
+                    </div>
+                </Col>
+                <Col span={8}></Col>
+                {/*<Col span={6}>
+                <SignIn/>
+            </Col>*/}
+            </Row>
+
+            {/*<Row>
+                <Col span={24}><p></p></Col>
+            </Row>*/}
+
+            <Row>
+                <p></p>
+            </Row>
+            <Row>
+                <p></p>
+                <Footer />
+            </Row>
+        </div>
     )
 }

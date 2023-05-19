e4e51f236a3aa0355cce75520ef5782dea971838
alecondou1704 <alecondou1704@gmail.com>
2023-05-19 01:03:19 -0400

    Implementando cerrar sesion responsive con pointer incluyendo su icono

--- bettystore/src/components/AppHeader/AppHeader.jsx
+++ bettystore/src/components/AppHeader/AppHeader.jsx
@@ -1,8 +1,9 @@
 import { Button } from "antd";
-import { BgColorsOutlined } from '@ant-design/icons'
+import { BgColorsOutlined, UserOutlined, LogoutOutlined} from '@ant-design/icons'
 import './AppHeader.css'
 import { useContext } from 'react';
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { Popover } from "antd";
 
 export default function AppHeader(props){
     const {contextTheme, switchTheme} = useContext(ThemeContext)
@@ -12,10 +13,21 @@ export default function AppHeader(props){
         switchTheme()
     }
 
+    function redireccionarLogin() {
+        window.location.href = "/";
+    }
+
     return(
         <div className="App-container-header">
             <span><img className="App-image-content" style={{backgroundColor:contextTheme.background}} alt='PruebaImg' src='./assets/logoSuperiorBS (2).png'/></span>
             <span className="App-title-header" style={{color: contextTheme.color, background: contextTheme.background}}>Sistema para Administración de Tienda</span>
+            <span> 
+                <Popover placement="bottomRight" 
+                         trigger={'click'} 
+                         arrow={false} 
+                         content={<a onClick={() => redireccionarLogin()} ><LogoutOutlined /> Cerrar Sesión</a>} >
+                             <span className="ocultar-nombre">Fernando</span><UserOutlined style={{fontSize:'24px',cursor:"pointer"}}/></Popover> 
+            </span>
             {/*<Button className="App-context-button" shape="circle" onClick={onClickSwithButton} icon={<BgColorsOutlined />}></Button>*/}
         </div>
     )
